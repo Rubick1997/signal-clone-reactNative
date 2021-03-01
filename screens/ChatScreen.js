e4e56b3f4f2a2fc33b99085e175b8419d52ec474
Rustam <rustam905@gmail.com>
2021-03-01 14:41:55 -0800

    Keyboard chat

--- screens/ChatScreen.js
+++ screens/ChatScreen.js
@@ -1,9 +1,22 @@
-import React, { useLayoutEffect } from "react";
-import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
+import React, { useLayoutEffect, useState } from "react";
+import {
+	StyleSheet,
+	Text,
+	View,
+	TouchableOpacity,
+	Platform,
+	KeyboardAvoidingView,
+	SafeAreaView,
+	ScrollView,
+} from "react-native";
 import { Avatar } from "react-native-elements";
 import { AntDesign, FontAwesome, Ionicons } from "@expo/vector-icons";
+import { StatusBar } from "expo-status-bar";
+import { TextInput } from "react-native";
 
 const ChatScreen = ({ navigation, route }) => {
+	const [input, setInput] = useState("");
+
 	useLayoutEffect(() => {
 		navigation.setOptions({
 			headerBackTitleVisible: false,
@@ -29,16 +42,78 @@ const ChatScreen = ({ navigation, route }) => {
 					<AntDesign name='arrowleft' size={24} color='white' />
 				</TouchableOpacity>
 			),
+			headerRight: () => (
+				<View
+					style={{
+						flexDirection: "row",
+						justifyContent: "space-between",
+						width: 80,
+						marginRight: 20,
+					}}>
+					<TouchableOpacity>
+						<FontAwesome name='video-camera' size={24} color='white' />
+					</TouchableOpacity>
+					<TouchableOpacity>
+						<Ionicons name='call' size={24} color='white' />
+					</TouchableOpacity>
+				</View>
+			),
 		});
 	}, [navigation]);
 
+	const sendMessage = () => {};
+
 	return (
-		<View>
-			<Text></Text>
-		</View>
+		<SafeAreaView
+			style={{
+				flex: 1,
+				backgroundColor: "white",
+			}}>
+			<StatusBar style='light' />
+			<KeyboardAvoidingView
+				behavior={Platform.OS === "ios" ? "padding" : "height"}
+				style={styles.container}
+				keyboardVerticalOffset={90}>
+				<>
+					<ScrollView></ScrollView>
+					<View style={styles.footer}>
+						<TextInput
+							placeholder='Signal Message'
+							style={styles.textInput}
+							value={input}
+							onChangeText={(text) => setInput(text)}
+						/>
+						<TouchableOpacity onPress={sendMessage} activeOpacity={0.5}>
+							<Ionicons name='send' size={24} color='#2B68E6' />
+						</TouchableOpacity>
+					</View>
+				</>
+			</KeyboardAvoidingView>
+		</SafeAreaView>
 	);
 };
 
 export default ChatScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+	},
+	footer: {
+		flexDirection: "row",
+		alignItems: "center",
+		width: "100%",
+		padding: 15,
+	},
+	textInput: {
+		bottom: 0,
+		height: 40,
+		flex: 1,
+		marginRight: 15,
+		borderColor: "transparent",
+		backgroundColor: "#ECECEC",
+		padding: 10,
+		color: "grey",
+		borderRadius: 30,
+	},
+});
