b4ab141829b4ec1ed8a1afe35db3f9e1fb81f3c1
Rustam <rustam905@gmail.com>
2021-02-28 20:21:31 -0800

    Entering the chat

--- screens/ChatScreen.js
+++ screens/ChatScreen.js
@@ -1,6 +1,7 @@
 import React, { useLayoutEffect } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
 import { Avatar } from "react-native-elements";
+import { AntDesign, FontAwesome, Ionicons } from "@expo/vector-icons";
 
 const ChatScreen = ({ navigation, route }) => {
 	useLayoutEffect(() => {
@@ -9,9 +10,24 @@ const ChatScreen = ({ navigation, route }) => {
 			headerTitleAlign: "left",
 			headerTitle: () => (
 				<View style={{ flexDirection: "row", alignItems: "center" }}>
-					<Avatar rounded />
-					<Text>{route.params.chatName}</Text>
+					<Avatar
+						rounded
+						source={{
+							uri:
+								"https://thumbs.dreamstime.com/b/default-avatar-photo-placeholder-profile-icon-eps-file-easy-to-edit-default-avatar-photo-placeholder-profile-icon-124557887.jpg",
+						}}
+					/>
+					<Text style={{ color: "white", marginLeft: 10, fontWeight: "700" }}>
+						{route.params.chatName}
+					</Text>
 				</View>
+			),
+			headerLeft: () => (
+				<TouchableOpacity
+					style={{ marginLeft: 10 }}
+					onPress={navigation.goBack}>
+					<AntDesign name='arrowleft' size={24} color='white' />
+				</TouchableOpacity>
 			),
 		});
 	}, [navigation]);
